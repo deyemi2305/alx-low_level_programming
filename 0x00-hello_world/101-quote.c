a3221e7739ff1e58b2970a2ab6e728231f1507dc
deyemi2305 <adeyemiibukun05@gmail.com>
2022-09-08 19:09:51 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Prints the string in the put function
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful\"- Dora Korpar,2015-10-19\n", 59);
+	printf(2, "and that piece of art is useful\"- Dora Korpar,2015-10-19\n",59);
 	return (1);
 }
 
