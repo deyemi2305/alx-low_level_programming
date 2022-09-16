a0772d504561e55a33bf0940cf9b0f6b0d446652
deyemi2305 <adeyemiibukun05@gmail.com>
2022-09-15 17:17:52 -0700

    commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,15 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
+
 /*
  * while (i < 10)
  * {
+ * infinite loop - No increase of variable
  * putchar(i);
  * }
  */
 printf("infinite loop avoided! \\o/\n");
+
 return (0);
 }
