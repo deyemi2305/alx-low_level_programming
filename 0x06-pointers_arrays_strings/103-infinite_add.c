4651178fa003ac1794e779bd1dddb64b937bcaa1
deyemi2305 <adeyemiibukun05@gmail.com>
2022-09-21 17:19:52 -0700

    infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -16,10 +16,6 @@
  * Return: returns pointer to result
  */
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-}
-
 /**
  * add_strings - Adds the numbers stored in two strings.
  * @n1: the string containing the first number to be added.
@@ -35,7 +31,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 	int num, tens = 0;
 
-	for (; *n1 && *n2; n1--; r_index++)
+	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
 		num = (*n1 - '0') + (*n2 - '0');
 		num += tens;
@@ -43,18 +39,29 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		tens = num / 10;
 	}
 
-	for (; *n1; n1--; r_index++)
+	for (; *n1; n1--, r_index--)
 	{
-		num = *(n1 - '0') + tens;
+		num = (*n1 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
 
-	for (; *n2; n2--; r_index--)
+	for (; *n2; n2--, r_index--)
 	{
 		num = (*n2 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
 
+	if (tens && r_index >= 0)
+	{
+		*(r + r_index) = (tens % 10) + '0';
+		return (r + r_index);
+	}
+
+
+	else if (tens && r_index < 0)
+		return (0);
+
+	return (r + r_index + 1);
 }
