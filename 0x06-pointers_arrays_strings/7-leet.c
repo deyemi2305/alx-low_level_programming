b4e81c324f4b8e3a49eb0f209be6fed88961c44e
deyemi2305 <adeyemiibukun05@gmail.com>
2022-09-22 06:21:04 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,46 +1,27 @@
 #include "main.h"
 
 /**
- * leet - leet encoder
- * @str: string to be encoded
+ * leet - encode into 1337speak
+ * @n: input value
  *
- * Return: address of the encoded string
+ * Return: n value
  */
 
-char *leet(char *str)
+char *leet(char *n)
 {
-	int i = 0;
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	while (str[i] != '\0')
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		str[i] = transform(str[i]);
-		i++;
-	}
-	return (str);
-}
-
-/**
- * transform - helper function to map a letter with it's leet encoding
- * @x: char to be encoded
- *
- * Return: the encoded char
- */
-
-char transform(char x)
-{
-	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
-	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
-	int i = 0;
-	char replacement = x;
-
-	while (i < 8)
-	{
-		if (x == mapping_low[i] || x == mapping_upper[i])
+		for (j = 0; j < 10; j++)
 		{
-			replacement = i + '0';
-			break;
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
 		}
-		i++;
 	}
-	return (replacement);
+	return (n);
 }
